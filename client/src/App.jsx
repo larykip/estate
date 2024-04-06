14cfd09102c67551721b2ab52b672cdec22e4e08
hillary-254 <123866475+hillary-254@users.noreply.github.com>
2024-04-06 13:13:30 +0300

    created the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/' className='text-slate-700 hover:underline'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-500'>KingFish</span>
+                    <span className='text-slate-700'>Propertiers</span>
+                </h1>
+            </Link>
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <FaSearch className='text-slate-600'/>
+            </form>
+            <ul className='flex gap-4'>
+                <Link to='/' className='text-slate-700 hover:underline'>
+                    <li className='text-slate-700 hover:underline hidden sm:inline'>Home</li>
+                </Link>
+                <Link to='/about' className='text-slate-700 hover:underline'>
+                    <li className='text-slate-700 hover:underline hidden sm:inline'>About</li>
+                </Link>
+                <Link to='/sign-in' className='text-slate-700 hover:underline'>
+                    <li className='text-slate-700 hover:underline'>Sign In</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
